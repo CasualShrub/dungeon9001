8f3d31a5f6e093cdc39857e4f22b9705ce57471a
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-05-12 20:09:24 +0800

    Add some game states

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import pygame
 
 from constants import *
 
+EXPLORING = "exploring"
+BATTLE    = "battle"
+GAME_OVER = "game_over"
+VICTORY = "victory"
+
 class Game:
     def __init__(self):
         pygame.init()
