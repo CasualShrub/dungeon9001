5e8e89c391c8864c6d3bc54361dab3877dcbf4e3
CasualShrub <120472172+CasualShrub@users.noreply.github.com>
2026-05-12 20:12:17 +0800

    Add some colors

--- constants.py
+++ constants.py
@@ -9,3 +9,13 @@ VICTORY = "victory"
 
 WALL = 0
 FLOOR = 1
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
+DARK_GRAY = (64, 64, 64)
+BLUE = (70, 130,180)
+RED = (200, 60, 60)
+GREEN = (60, 180, 60)
+YELLOW = (220, 200, 50)
+ORANGE = (220, 140, 50)
